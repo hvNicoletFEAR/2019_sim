20e88b5ec4577c5a879918818f707121a506ca12
maccopacco <maxdmax101@gmail.com>
2018-10-11 01:11:44 -0400

    commit 1

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import java.text.DecimalFormat;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -10,44 +12,58 @@ public class Robot extends TimedRobot {
 
   Timer timer = new Timer();
   Joystick joy = new Joystick(0);
-  Spark L1, L2;
-  DifferentialDrive drive;
+
+  double clearFirstStageMaxHeight = 5;
+  double clearFirstStageMinimumAngle = 35;
+
+  double targetHeight = 0;
+  double targetAngle = 0;
+  double elevatorHeight = 0;
+  double wristAngle = 0;
 
   @Override
   public void robotInit() {
-    L1 = new Spark(1);
-    L2 = new Spark(2);
-    
-    L2.setInverted(true);
-    
-    drive = new DifferentialDrive(L1,L2);
   }
 
-  
-  @Override
-  public void robotPeriodic() {
+  int scaleBetween(int unscaledNum, int minAllowed, int maxAllowed, int min, int max) {
+    return (maxAllowed - minAllowed) * (unscaledNum - min) / (max - min) + minAllowed;
   }
 
- 
-  @Override
-  public void autonomousInit() {
-    timer.stop();
-    timer.reset();
-    timer.start();
+  double scaleBetween(double unscaledNum, double minAllowed, double maxAllowed, double min, double max) {
+    return (maxAllowed - minAllowed) * (unscaledNum - min) / (max - min) + minAllowed;
   }
 
-  @Override
-  public void autonomousPeriodic() {
-    if (timer.get() <= 5)
-      drive.arcadeDrive(1.0,0.0);
-    else
-      drive.arcadeDrive(0,0);
-  }
-
-  
   @Override
   public void teleopPeriodic() {
-    drive.arcadeDrive(-joy.getRawAxis(0), joy.getRawAxis(4));
+    DecimalFormat df = new DecimalFormat("###.##");
+
+    targetHeight = scaleBetween(-joy.getRawAxis(0), 0, 10, -1, 1);
+    targetHeight = Double.valueOf(df.format(targetHeight));
+
+    targetAngle = scaleBetween(joy.getRawAxis(3), 0, 180, -1, 1);
+    targetAngle = Double.valueOf(df.format(targetAngle));
+
+    boolean badZone = targetHeight >= clearFirstStageMaxHeight && targetAngle < clearFirstStageMinimumAngle;
+
+    if (badZone) {
+
+      //folded, want to go high, let wrist move first
+      if (wristAngle < clearFirstStageMinimumAngle && targetHeight > clearFirstStageMaxHeight) {
+        elevatorHeight = Math.min(targetHeight, clearFirstStageMaxHeight);
+        wristAngle = targetAngle;
+      
+      //want to fold, dont let wrist move until elevator good
+      } else {
+        elevatorHeight = targetHeight;
+        wristAngle = Math.max(targetAngle, clearFirstStageMinimumAngle);
+      }
+    } else {
+      elevatorHeight = targetHeight;
+      wristAngle = targetAngle;
+    }
+
+    System.out.println("Elevator [" + elevatorHeight + "]   [" + targetHeight + "]" + "\t\tWrist [" + wristAngle
+        + "]   [" + targetAngle + "]" + "\t\tCorrect zone: " + !badZone);
   }
 
   @Override
